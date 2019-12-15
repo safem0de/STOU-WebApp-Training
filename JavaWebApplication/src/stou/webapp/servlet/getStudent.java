101d04703f8dd2f2537c7b3b977dcb4b5fec4d05
STOU <STOU@COMP3-16.stou.local>
2019-12-15 20:33:08 +0700

    ..

--- JavaWebApplication/src/stou/webapp/servlet/getStudent.java
+++ JavaWebApplication/src/stou/webapp/servlet/getStudent.java
@@ -30,7 +30,7 @@ public class getStudent extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		int id = Integer.parseInt(request.getParameter("img"));
 		StudentImgManager sim = new StudentImgManager();
 		
