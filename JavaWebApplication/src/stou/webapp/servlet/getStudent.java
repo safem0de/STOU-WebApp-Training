9a363459a3338e79eef546fa28b9d27e023ba479
STOU <STOU@COMP3-16.stou.local>
2019-12-15 20:25:27 +0700

    getStudent

--- JavaWebApplication/src/stou/webapp/servlet/getStudent.java
+++ JavaWebApplication/src/stou/webapp/servlet/getStudent.java
@@ -14,13 +14,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class viewStudent
  */
 @WebServlet("/viewStudent")
-public class viewStudent extends HttpServlet {
+public class getStudent extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public viewStudent() {
+    public getStudent() {
         super();
         // TODO Auto-generated constructor stub
     }
